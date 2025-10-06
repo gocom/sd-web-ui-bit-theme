54fb6041da2e14fba437c312fb72c9bbafc1769a
gocom <665186+gocom@users.noreply.github.com>
2025-10-06 21:13:20 +0000

    Build for master commit be46132

--- javascript/index.js
+++ javascript/index.js
@@ -60,6 +60,70 @@ const Image = `
 
 //# sourceMappingURL=image.js.map
 
+;// ../node_modules/lucide-static/dist/esm/icons/arrow-down-wide-narrow.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const ArrowDownWideNarrow = `
+<svg
+  class="lucide lucide-arrow-down-wide-narrow"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="m3 16 4 4 4-4" />
+  <path d="M7 20V4" />
+  <path d="M11 4h10" />
+  <path d="M11 8h7" />
+  <path d="M11 12h4" />
+</svg>
+`;
+
+
+//# sourceMappingURL=arrow-down-wide-narrow.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/arrow-up-narrow-wide.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const ArrowUpNarrowWide = `
+<svg
+  class="lucide lucide-arrow-up-narrow-wide"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="m3 8 4-4 4 4" />
+  <path d="M7 4v16" />
+  <path d="M11 12h4" />
+  <path d="M11 16h7" />
+  <path d="M11 20h10" />
+</svg>
+`;
+
+
+//# sourceMappingURL=arrow-up-narrow-wide.js.map
+
 ;// ./javascript/component/icons.ts
 
 /**
@@ -107,6 +171,8 @@ const replaceIcon = (elements, emojis, svg, size) => {
 /* harmony default export */ const icons = (() => {
     replaceIcon(document.querySelectorAll('.img-meta-ico, .copy_code_button'), [], Copy, 16);
     replaceIcon(document.querySelectorAll('.empty .icon'), [], Image, 36);
+    replaceIcon(document.querySelectorAll('button'), ['▼'], ArrowDownWideNarrow, 16);
+    replaceIcon(document.querySelectorAll('button'), ['▲'], ArrowUpNarrowWide, 16);
 });
 
 ;// ../node_modules/lucide-static/dist/esm/icons/eye-off.js
