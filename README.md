c282c708a6d96f66e4e5e2ee682ef5309555f785
gocom <665186+gocom@users.noreply.github.com>
2025-10-16 19:15:59 +0000

    Build for 0.5.0 commit 93f54ed

---
+++ README.md
@@ -0,0 +1,49 @@
+🎨 Personal styles for Stable Diffusion Web UI
+=====
+
+A collection of personal addon styles for the [AUTOMATIC1111's Stable Diffusion web UI](https://github.com/AUTOMATIC1111/stable-diffusion-webui)
+and the [Lobe Theme](https://github.com/lobehub/sd-webui-lobe-theme), packaged as an extension. This extension introduces typography styles and
+miscellaneous tweaks.
+
+📃 Requirements
+-----
+
+The extension requires:
+
+* [Stable Diffusion web UI v1.10.1](https://github.com/AUTOMATIC1111/stable-diffusion-webui/releases/tag/v1.10.1)
+* [Lobe Theme v3.5.4](https://github.com/lobehub/sd-webui-lobe-theme)
+
+🍙 Supported
+-----
+
+Styles are also provided for extensions:
+
+* [sd-model-preview-xd](https://github.com/CurtisDS/sd-model-preview-xd)
+* [stable-diffusion-webui-images-browser](https://github.com/AlUlkesh/stable-diffusion-webui-images-browser)
+
+⚡ Install
+-----
+
+The repository can be installed as an extension to Stable Diffusion web UI.
+
+### Option 1: Download pre-packaged archive
+
+Download pre-packaged [sd-web-ui-bit-theme.zip](https://github.com/gocom/sd-web-ui-bit-theme/releases/latest/download/sd-web-ui-bit-theme.zip), and extract it into your **extensions** directory under
+Stable Diffusion Web UI's installation directory.
+
+### Option 2: Clone build branch
+
+Clone from command line, or via WEB UI, the wished **build** branch, which contain the built extension. The following
+would install the main development branch, feel free to substitute it with a specific release version:
+
+```shell
+$ cd /path/to/stable-diffusion-webui/extensions/
+$ git clone https://github.com/gocom/sd-web-ui-bit-theme.git --branch build/master
+```
+
+This can also be done from the web GUI by providing the same details in the interface.
+
+🛠️ Development
+-----
+
+See [CONTRIBUTING.md](https://github.com/gocom/sd-web-ui-bit-theme/blob/master/CONTRIBUTING.md).
---
+++ javascript/index.js
@@ -0,0 +1,405 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+;// ../node_modules/lucide-static/dist/esm/icons/copy.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const Copy = `
+<svg
+  class="lucide lucide-copy"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+  <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+</svg>
+`;
+
+
+//# sourceMappingURL=copy.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/image.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const Image = `
+<svg
+  class="lucide lucide-image"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
+  <circle cx="9" cy="9" r="2" />
+  <path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21" />
+</svg>
+`;
+
+
+//# sourceMappingURL=image.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/eye.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const Eye = `
+<svg
+  class="lucide lucide-eye"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+  <circle cx="12" cy="12" r="3" />
+</svg>
+`;
+
+
+//# sourceMappingURL=eye.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/square-pen.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const SquarePen = `
+<svg
+  class="lucide lucide-square-pen"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+  <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
+</svg>
+`;
+
+
+//# sourceMappingURL=square-pen.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/info.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const Info = `
+<svg
+  class="lucide lucide-info"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <circle cx="12" cy="12" r="10" />
+  <path d="M12 16v-4" />
+  <path d="M12 8h.01" />
+</svg>
+`;
+
+
+//# sourceMappingURL=info.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/circle-x.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const CircleX = `
+<svg
+  class="lucide lucide-circle-x"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <circle cx="12" cy="12" r="10" />
+  <path d="m15 9-6 6" />
+  <path d="m9 9 6 6" />
+</svg>
+`;
+
+
+//# sourceMappingURL=circle-x.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/arrow-down-wide-narrow.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const ArrowDownWideNarrow = `
+<svg
+  class="lucide lucide-arrow-down-wide-narrow"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="m3 16 4 4 4-4" />
+  <path d="M7 20V4" />
+  <path d="M11 4h10" />
+  <path d="M11 8h7" />
+  <path d="M11 12h4" />
+</svg>
+`;
+
+
+//# sourceMappingURL=arrow-down-wide-narrow.js.map
+
+;// ../node_modules/lucide-static/dist/esm/icons/arrow-up-narrow-wide.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const ArrowUpNarrowWide = `
+<svg
+  class="lucide lucide-arrow-up-narrow-wide"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="m3 8 4-4 4 4" />
+  <path d="M7 4v16" />
+  <path d="M11 12h4" />
+  <path d="M11 16h7" />
+  <path d="M11 20h10" />
+</svg>
+`;
+
+
+//# sourceMappingURL=arrow-up-narrow-wide.js.map
+
+;// ./javascript/component/icons.ts
+
+/**
+ * Insert the given SVG icon to the given element.
+ */
+const insertIcon = (element, svg, size) => {
+    element.innerHTML = svg
+        .replace(/width="24"/gi, `width="${size}"`)
+        .replace(/height="24"/gi, `height="${size}"`);
+    element.classList.add('has-icon', 'anticon');
+};
+/**
+ * Replaces the element with the specified icon.
+ *
+ * If `contents` is not an empty array, check that the
+ * element contains any of the given text contents. If it does,
+ * replaces the element with the given svg.
+ */
+const replaceElementWithIcon = (element, contents, svg, size) => {
+    var _a;
+    if (element.classList.contains('has-icon')) {
+        return;
+    }
+    if (!contents.length) {
+        insertIcon(element, svg, size);
+        return;
+    }
+    if ((element === null || element === void 0 ? void 0 : element.textContent) && svg) {
+        for (const content of contents) {
+            if ((_a = element === null || element === void 0 ? void 0 : element.textContent) === null || _a === void 0 ? void 0 : _a.includes(content)) {
+                insertIcon(element, svg, size);
+                return;
+            }
+        }
+    }
+};
+/**
+ * Replaces the specified icon.
+ */
+const replaceIcon = (elements, emojis, svg, size) => {
+    for (const element of elements) {
+        replaceElementWithIcon(element, emojis, svg, size);
+    }
+};
+/* harmony default export */ const icons = (() => {
+    replaceIcon(document.querySelectorAll('.img-meta-ico, .copy_code_button'), [], Copy, 16);
+    replaceIcon(document.querySelectorAll('.empty .icon'), [], Image, 36);
+    replaceIcon(document.querySelectorAll('.extra-network-cards .card .previewXD-button'), [], Eye, 16);
+    replaceIcon(document.querySelectorAll('.extra-network-pane .copy-path-button'), [], Copy, 16);
+    replaceIcon(document.querySelectorAll('.extra-network-pane .edit-button'), [], SquarePen, 16);
+    replaceIcon(document.querySelectorAll('.extra-networks .metadata-button'), [], Info, 16);
+    replaceIcon(document.querySelectorAll('.img-properties-list summary'), [], `${Info}${CircleX}`, 16);
+    replaceIcon(document.querySelectorAll('button'), ['▼'], ArrowDownWideNarrow, 16);
+    replaceIcon(document.querySelectorAll('button'), ['▲'], ArrowUpNarrowWide, 16);
+});
+
+;// ../node_modules/lucide-static/dist/esm/icons/eye-off.js
+/**
+ * @license lucide-static v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const EyeOff = `
+<svg
+  class="lucide lucide-eye-off"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" />
+  <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
+  <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" />
+  <path d="m2 2 20 20" />
+</svg>
+`;
+
+
+//# sourceMappingURL=eye-off.js.map
+
+;// ./javascript/component/livePreviewIcon.ts
+
+
+/**
+ * Inject icon to the clone based on the original button content.
+ */
+const injectIcon = (button, clone) => {
+    clone.title = button.title;
+    const svg = button.innerText === '🗆'
+        ? EyeOff
+        : Eye;
+    insertIcon(clone, svg, 36);
+};
+/* harmony default export */ const livePreviewIcon = (() => {
+    if (!document.querySelector('.modal-toggleLivePreview-clone')) {
+        const button = document.querySelector('.modalToggleLivePreview');
+        if (button) {
+            const clone = document.createElement('span');
+            clone.classList.add('modal-toggleLivePreview-clone', 'cursor');
+            injectIcon(button, clone);
+            clone.onclick = (event) => {
+                event.preventDefault();
+                event.stopPropagation();
+                button.dispatchEvent(new Event('click'));
+                injectIcon(button, clone);
+            };
+            button.after(clone);
+        }
+    }
+});
+
+;// ./javascript/component/favicon.ts
+/**
+ * Replaces favicon with the configured logo.
+ */
+const replaceFavicon = () => {
+    var _a;
+    const logo = (_a = document.querySelector('a .ant-space-item > img[alt="logo"]')) === null || _a === void 0 ? void 0 : _a.src;
+    if (logo) {
+        const links = document.querySelectorAll('link[rel="shortcut icon"], link[rel="apple-touch-icon"], link[rel="icon"]');
+        for (const link of links) {
+            if (!link.classList.contains('has-logo')) {
+                link.href = logo;
+                link.removeAttribute('type');
+                link.classList.add('has-logo');
+            }
+        }
+    }
+};
+/* harmony default export */ const favicon = (replaceFavicon);
+
+;// ./javascript/index.ts
+
+
+
+/**
+ * Inject scripts and DOM modifiers.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    icons();
+    livePreviewIcon();
+    favicon();
+    const observer = new MutationObserver(() => {
+        icons();
+        favicon();
+    });
+    observer.observe(gradioApp(), {
+        subtree: true,
+        childList: true,
+    });
+});
+
+/******/ })()
+;
